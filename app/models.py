5bcba3acab7812ab4a74a2c954a975456470ad55
German Martinez <germand_m@hotmail.com>
2019-07-05 19:51:03 -0300

    add Sensor and DebugCode tables

--- app/models.py
+++ app/models.py
@@ -4,6 +4,36 @@ from app.database import Base
 from sqlalchemy import Column, Integer, JSON, String, Boolean, DateTime, BigInteger
 
 
+class Sensor(Base):
+    __tablename__ = "sensor"
+    id = Column(BigInteger, autoincrement='auto', primary_key=True)
+    device_id = Column(String, primary_key=True)
+    mac_addr = Column(String)
+    type = Column(String)
+    location = Column(String)
+    description = Column(String)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+
+    def __init__(self, device_id, mac_addr, type, location, description=''):
+        self.device_id = device_id
+        self.mac_addr = mac_addr
+        self.type = type
+        self.location = location
+        self.description = description
+
+    @property
+    def serialize(self):
+        return dict(device_id=self.device_id,
+                    mac_addr=self.mac_addr,
+                    type=self.type,
+                    location=self.location,
+                    description=self.description,
+                    created_at=self.created_at)
+
+    def __repr__(self):
+        return f'<Sensor id:{self.device_id}, type:{self.net_key}>'
+
+
 class SensorData(Base):
     __tablename__ = 'sensordata'
     id = Column(BigInteger, autoincrement='auto', primary_key=True)
@@ -19,11 +49,11 @@ class SensorData(Base):
         self.net_key = net_key
         self.device_id = device_id
         self.channels = channel
-        self.value =  value
+        self.value = value
 
 
     def __repr__(self):
-        return f'<SensorData device_id:{self.device_id}, net_key:{self.net_key}>'
+        return f'<SensorData id:{self.id}, device_id:{self.device_id}, net_key:{self.net_key}>'
 
     def decode(self, payload):
         pass
@@ -35,15 +65,14 @@ class SensorData(Base):
                     net_key=self.net_key,
                     channel=self.channel,
                     value=self.value,
-                    created_at=self.created_at
-                    )
+                    created_at=self.created_at)
 
 
 class SensorDebug(Base):
     __tablename__ = "debug"
-    id = Column(Integer, autoincrement='auto', primary_key=True)
+    id = Column(BigInteger, autoincrement='auto', primary_key=True)
     device_id = Column(String)
-    code = Column(Integer)
+    code = Column(String)
     message = Column(String)
     created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
 
@@ -53,12 +82,32 @@ class SensorDebug(Base):
         self.message = message
 
     def __repr__(self):
-        return f'<SensorDebug device_id:{self.device_id}, code:{self.code}, message:{self.message}, created_at:{self.created_at}>'
+        return f'<SensorDebug id:{self.id}, device_id:{self.device_id}, code:{self.code}, message:{self.message}, created_at:{self.created_at}>'
 
     @property
     def serialize(self):
         return dict(device_id=self.device_id,
                     code=self.code,
                     message=self.message,
-                    created_at=self.created_at
-                    )
+                    created_at=self.created_at)
+
+
+class DebugCode(Base):
+    __tablename__ = "debugcode"
+    id = Column(BigInteger, autoincrement='auto', primary_key=True)
+    code = Column(String)
+    description=Column(String)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+
+    def __init__(self, code, description):
+        self.code = code
+        self.description = description
+
+    @property
+    def serialize(self):
+        return dict(code=self.code,
+                    description=self.description,
+                    created_at=self.created_at)
+
+    def __repr__(self):
+        return f'<SensorDebug id:{id}, code:{self.code}, description:{self.description}>'
