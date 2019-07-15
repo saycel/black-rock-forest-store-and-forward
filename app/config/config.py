e8d33e087b7b1e480084d545f8acb9be2cb6d862
German Martinez <germand_m@hotmail.com>
2019-07-15 09:03:33 -0300

    fix database ip address

--- app/config/config.py
+++ app/config/config.py
@@ -1,5 +1,5 @@
 DEV_DATABASE_URI = 'postgresql://postgres:postgres@localhost/black_forest'
-PROD_DATABASE_URI = 'postgresql://postgres:postgres@10.0.0.5/black_forest'
+PROD_DATABASE_URI = 'postgresql://postgres:postgres@localhost/black_forest'
 USE_API_STUBS = True
 SQLALCHEMY_ECHO = True
 DEBUG = True
