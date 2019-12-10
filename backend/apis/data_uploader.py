ae5316b317f846e34f427233fd52ec56758d2273
German Martinez <germand_m@hotmail.com>
2019-12-10 19:31:49 -0300

    hardcode user for csv uploader

--- backend/apis/data_uploader.py
+++ backend/apis/data_uploader.py
@@ -12,12 +12,12 @@ api = Namespace("data_uploader", description="Upload sensor data from csv files"
 
 @api.route("/data")
 class Data(Resource):
-    @auth_needed
+    # @auth_needed
     @disable_for_raspberry
     def put(self):
         start_t = datetime.utcnow()
         inserted_records = CsvDataService().insert_many_from_http(
-            request.data, g.current_user.id
+            request.data, "999" # TODO  change me when you put back security
         )
         took = datetime.utcnow() - start_t
         return {"took": str(took), "inserted": inserted_records, "status": "success"}
